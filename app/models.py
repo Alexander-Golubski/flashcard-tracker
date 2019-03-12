252952bddabddbb7e2259644e0cfe5d14b22de75
Alexander Golubski <algolubski@gmail.com>
2019-03-12 15:04:47 -0500

    added Card and Deck models

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(UserMixin, db.Model):
     last_name = db.Column(db.String(30))
     email = db.Column(db.String(40), unique=True)
     password_hash = db.Column(db.String(128))
-    #cards = db.relationship("Card", backref="owner") 
+    decks = db.relationship('Deck', backref='owner') 
     
     def __init__(self, first_name, last_name, email, password_hash):
         self.first_name = first_name
@@ -32,8 +32,38 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<ID: {} {} {}>'.format(self.id, self.first_name, self.last_name)
+        return '<ID: {}, {} {}>'.format(self.id, self.first_name, self.last_name)
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+class Deck(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    cards = db.relationship('Card', backref='deck')
+
+    def __init__(self, name, owner):
+        self.name = name
+        self.owner = owner
+    
+    def __repr__(self):
+        return '<ID: {}, Name: {}, Owner: {}>'.format(self.id, self.name, self.owner)
+
+class Card(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    front = db.Column(db.String(1200))
+    back = db.Column(db.String(1200))
+    deck_id = db.Column(db.Integer, db.ForeignKey('deck.id'))
+
+    def __init__(self, front, back, deck):
+        self.front = front
+        self.back = back
+        self.deck = deck
+    
+    def __repr__(self):
+        return '<ID: {}, Front: {}, Back: {}, Deck: {}>'.format(self.id, 
+                                         self.front, self.back, self.deck)
