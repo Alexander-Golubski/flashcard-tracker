10f37906134f494ff83ddd1d83248f43be699ffd
Alexander Golubski <algolubski@gmail.com>
2019-03-17 19:47:04 -0500

    sqlalchemy_echo to false

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # key.py currently ignored by git
